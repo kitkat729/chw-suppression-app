01e7bf087bb02dc9988e6454831b4f75e05ec305
Hoi Yuen <hoikityuen@gmail.com>
2018-06-25 04:47:43 -0400

    feature-#3 setup data store and app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux'
-//import configureStore from './configureStore'
+import configureStore from './configureStore'
 
 import './App.css';
 
+import SuppressionListContainer from './containers/SuppressionListContainer'
+
 const App = (props) => {
 
-    //const store = configureStore();
+    const store = configureStore();
 
     return (
       <div className="App">
-
+        <Provider store={store}>
+          <SuppressionListContainer />
+        </Provider>
       </div>
-    );
+    )
 }
 
 export default App;
