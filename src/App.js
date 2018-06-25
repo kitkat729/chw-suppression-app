775bb041c0259d369aad343766e0a7e5ba95addd
Hoi Yuen <hoikityuen@gmail.com>
2018-06-25 00:35:46 -0400

    structure-#1 added docker files and clean app.js
    structure-#1 added docker files and clean app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux'
+//import configureStore from './configureStore'
+
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = (props) => {
+
+    //const store = configureStore();
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
-  }
 }
 
 export default App;
